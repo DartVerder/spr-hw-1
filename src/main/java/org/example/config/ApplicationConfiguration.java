eff27a75f42604acc236e65176b269c1bb28c3db
Vera Beketova <v.beketova@haulmont.com>
2021-07-14 14:53:52 +0400

    remove Service and Repository #7

--- src/main/java/org/example/config/ApplicationConfiguration.java
+++ src/main/java/org/example/config/ApplicationConfiguration.java
@@ -1,11 +1,6 @@
 package org.example.config;
 
-import org.example.pool.AnnotationConnectionPool;
 import org.example.pool.ApplicationConnectionPool;
-import org.example.pool.XMLConnectionPool;
-import org.example.property.ApplicationPropertySourcesPlaceholderConfigurer;
-import org.example.repository.UserRepository;
-import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,16 +19,6 @@ public class ApplicationConfiguration {
   @Bean
   public ApplicationConnectionPool connectionPool(@Value("${login}") String login, @Value("${password}") String password, @Value("${url}") String url) {
     return new ApplicationConnectionPool(login, password, url);
-  }
-
-  @Bean
-  public UserService userService(UserRepository repository) {
-    return new UserService(repository);
-  }
-
-  @Bean
-  public UserRepository userRepository() {
-    return new UserRepository();
   }
 
  }
